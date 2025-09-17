53b14cf43ee3f775feff887457446b780dd3541e
andrewwbaranoff-ship-it <andrewwbaranoff@gmail.com>
2025-09-17 22:58:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://akbconf.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
